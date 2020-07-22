8c27af2b9e9c38c7fc688bdf5b26fff833b3040c
rebeccasun <sun.ruoy@husky.neu.edu>
2020-07-22 09:50:37 -0700

    service, clients and zuul built

---
+++ PhotoAppApiAccountManagement/src/test/java/com/appsdeveloperblog/photoapp/api/account/PhotoAppApiAccountManagement/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.account.PhotoAppApiAccountManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PhotoAppApiZuulAPIGateway/src/test/java/com/appsdeveloperblog/photoapp/api/gateway/PhotoAppApiZuulAPIGateway/PhotoAppApiZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.gateway.PhotoAppApiZuulAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
