680145f04adc370354dfa6028eeefa466ca8aa43
rebeccasun <sun.ruoy@husky.neu.edu>
2020-07-29 12:49:06 -0700

    secure endpoints by preauthorize annotation

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controller/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controller/UsersController.java
@@ -13,6 +13,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class UsersController {
   }
 
   @GetMapping(value="/{userId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @PreAuthorize("principal == #userId")
   public ResponseEntity<UserResponseModel> getUser(@PathVariable("userId") String userId) {
 
     UserDto userDto = usersService.getUserByUserId(userId);
