9ee8d4124e5471d219a4a04509260d833693f7e4
rebeccasun <sun.ruoy@husky.neu.edu>
2020-07-29 11:21:16 -0700

    turn off logger when production

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.photoapp.api.users.PhotoAppApiUsers;
 
 import com.appsdeveloperblog.photoapp.api.users.shared.FeignErrorDecoder;
 import feign.Logger;
+import feign.Logger.Level;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -9,6 +11,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +21,9 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
+
+	@Autowired
+	Environment environment;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
@@ -34,10 +41,37 @@ public class PhotoAppApiUsersApplication {
 	}
 
 	@Bean
-	Logger.Level feignLoggerLover() {
+	@Profile("production")
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.NONE;
+	}
+
+	@Bean
+	@Profile("!production")
+	Logger.Level feignDefaultLoggerLevel() {
 		return Logger.Level.FULL;
 	}
 
+	@Bean
+	@Profile("production")
+	public String createProductionBean() {
+		System.out.println("Production bean created." + environment.getProperty("myapplication.environment"));
+		return "Production bean";
+	}
+
+	@Bean
+	@Profile("!production")
+	public String createNotProductionBean() {
+		System.out.println("Not Production bean created." + environment.getProperty("myapplication.environment"));
+		return "Production bean";
+	}
+
+	@Bean
+	@Profile("default")
+	public String createDevelopmentBean() {
+		System.out.println("Development bean created." + environment.getProperty("myapplication.environment"));
+		return "Development bean";
+	}
 	/*
 	@Bean
 	public FeignErrorDecoder getFeignErrorDecoder() {
